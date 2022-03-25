7ae4cdd51b1c15a6f41195999236ba16b223afaf
Rafael Arcanjo <rafarcanjoatos@gmail.com>
2022-03-25 17:50:22 -0300

    Update index.php

--- 1-wordle/index.php
+++ 1-wordle/index.php
@@ -35,7 +35,7 @@ echo '<div class="col-10">';
         echo '</div>';
     echo '<br>';
 //}
-echo '<input type="button" class="button" onclick="validateWord();">Enviar</input>';
+echo '<input type="button" value="Enviar" class="button" onclick="validateWord();"></input>';
 echo '</div>';
 echo '</div>';
 

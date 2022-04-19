c8d582e9ea89a0166eccf995d585fff02162df2e
Rafael Arcanjo <rafarcanjoatos@gmail.com>
2022-04-19 00:46:49 -0300

    Engine Melhorada
    Validação Funcionando

--- 1-wordle/js/ValidateWord.js
+++ 1-wordle/js/ValidateWord.js
@@ -1,26 +1,32 @@
-function validateWord(leter_1,leter_2,leter_3,leter_4,leter_5) {
+function validateWord(leter_1,leter_2,leter_3,leter_4,leter_5,line) {
     // Concatenate word of user
     value2 = leter_1.concat(leter_2,leter_3,leter_4,leter_5);
-
-    return startValidate(value,value2);
+    return startValidate(value,value2,line);
 }
 
-function startValidate(value,value2){
-    const lettersOfDay = value.split("");
-    let lettersUser = value2.split("");
+function startValidate(value,value2,r){
+    const letersOfDay = value.split("");
+    let letersUser = value2.split("");
     let acertou;
     let errou;
 
-    console.log(lettersOfDay);
-    console.log(lettersUser);
+    console.log(letersOfDay);
+    console.log(letersUser);
 
-    for (let $i=0; $i<5; $i++){
-        if (lettersOfDay[$i]==lettersUser[$i]){
-            acertou += [$i+1]
-        }else{
-            errou += [$i+1]
+        for (let l=0; l<5; l++){
+            if (letersOfDay[l]==letersUser[l]){
+                acertou += [l+1]
+                document.getElementById('div_leter_'+[l+1]+'_'+r).style.backgroundColor = 'green';  
+                document.getElementById('leter_'+[l+1]+'_'+r).readOnly = true;        
+                
+            }else{
+                errou += [l+1]            
+                document.getElementById('div_leter_'+[l+1]+'_'+r).style.backgroundColor = 'red';  
+                document.getElementById('leter_'+[l+1]+'_'+r).readOnly = true;  
+            }
+            document.getElementById('button_'+r).style.display = 'none';
+            document.getElementById('button_'+(r+1)).style.display = 'true';  
         }
-    }
 
     console.log("Acertou: "+acertou)
     console.log("Errou: "+errou)
